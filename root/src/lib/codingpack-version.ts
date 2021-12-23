eb34defadf8b8cff18619821925ffca71286fcde
ktaicoder <ktaicodingblock.dev@gmail.com>
2021-12-23 17:57:02 +0900

    update codingpack version

--- root/src/lib/codingpack-version.ts
+++ root/src/lib/codingpack-version.ts
@@ -2,15 +2,15 @@ export const CODINGPACK = {
     rp3: {
         //version: 'v2.1.5',
         //date: '2021.12.07',
-        version: 'v2.1.6',
-        date: '2021.12.20',
-        fileName: 'kt_acp_rpi3__v2.1.6.zip',
+        version: 'v2.1.7',
+        date: '2021.12.23',
+        fileName: 'kt_acp_rpi3__v2.1.7.zip',
     },
     rp4: {
         // version: 'v3.3',
         // date: '2021.12.07',
-        version: 'v3.4',
-        date: '2021.12.20',
-        fileName: 'kt_acp_rpi4__v3.4.zip',
+        version: 'v3.5',
+        date: '2021.12.23',
+        fileName: 'kt_acp_rpi4__v3.5.zip',
     },
 }
